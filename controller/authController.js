c48e6a9fef048f2a5d79218b03ebe38e5dae991a
ktDeepak <deepakkt5555@gmail.com>
2022-11-01 16:57:41 +0530

    auth complete

---
+++ controller/authController.js
@@ -0,0 +1,117 @@
+const {StatusCodes} = require('http-status-codes')
+const User = require('../model/userModel')
+const bcrypt = require('bcryptjs')
+const { createAccessToken } = require('../util/token')
+const jwt = require('jsonwebtoken')
+
+const authController = {
+    register: async (req, res) => {
+        
+        try{
+            const { name, email, mobile, password } = req.body
+
+            const encPassword = await bcrypt.hash(password, 10)
+           // res.json({data: { name, email, mobile, password }})
+            const newUser = await User.create({
+                name,
+                email,
+                mobile,
+                password: encPassword
+            }) 
+            //res.json({data: newUser})
+           res.status(StatusCodes.OK).json({ msg: "user registered successfully", data: newUser })
+        }
+        catch(err){
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg: err.message})
+        }
+    },
+    login: async (req,res) =>{
+        try{
+            const { email, password } = req.body
+            //user exist or not
+            const extUser = await User.findOne({ email })
+            if(!extUser)
+                return res.status(StatusCodes.NOT_FOUND).json({ msg: "user doesnot exist..."})
+
+            //compare password
+            const isMatch = await bcrypt.compare(password, extUser.password)
+            if(!isMatch)
+                return res.status(StatusCodes.BAD_REQUEST).json({ msg: "password is not match"})
+
+            //generate token
+            const accessToken = createAccessToken({ _id: extUser._id })
+
+            // save token in cookie
+            res.cookie('refreshToken', accessToken, {
+                httpOnly: true,
+                signed: true,
+                path: `/api/v1/auth/refreshToken`,
+                maxAge: 1 * 24 * 60 * 60  * 1000
+            })
+            
+             res.json({ msg: "login successful", accessToken })
+        }catch(err){
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg: err.message})
+        }
+    },
+    logout: async (req,res) =>{
+  try{      
+            res.clearCookie('refreshToken', { path: `/api/v1/auth/refreshToken`})
+             res.json({msg: "logout"})
+        }catch(err){
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg: err.message})
+        }
+    },
+    refereshToken: async (req,res) =>{
+  try{
+        const rf = req.signedCookies.refreshToken
+
+        if(!rf)
+            return res.status(StatusCodes.BAD_REQUEST).json({ msg: "session expired, login again..."})
+
+        // valid user id or not
+        jwt.verify(rf, process.env.ACCESS_TOKEN_SECRET, (err, user)=>{
+
+            if(err)
+                return res.status(StatusCodes.BAD_REQUEST).json({ msg: "Invalid Access Token.. Login again "})
+            // valid token
+            const accessToken=createAccessToken({id:user._id})
+            res.json({ accessToken })
+        })
+        //res.json({ rf })
+        // res.json({msg: "refreshToken"})
+        }catch(err){
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg: err.message})
+        }
+    },
+
+    resetPassword: async (req,res) =>{
+  try{  
+    const id=req.user.id
+    const {oldPassword,newPassword}=req.body
+
+    //read user data
+    const extUser=await User.findById({_id:id})
+       if(!extUser)
+       return res.status(StatusCodes.NOT_FOUND).json({msg:"user does't exists."})
+
+    //compare password
+    const isMatch=await bcrypt.compare(oldPassword,extUser.password)   
+
+    if(!isMatch)
+    return res.status(StatusCodes.BAD_REQUEST).json({msg:"old password aren't match."})
+
+   //generate neew password hash
+    const passwordHash=await bcrypt.hash(newPassword,10)
+
+    //logic update
+    const output=await User.findByIdAndUpdate({_id:id},{password:passwordHash})
+
+         res.json({msg:"user password reset success",output})
+        }catch(err){
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg: err.message})
+        }
+    },
+
+}
+module.exports = authController
---
+++ route/authRoute.js
@@ -0,0 +1,16 @@
+const route = require('express').Router()
+const authController = require('../controller/authController')
+const auth = require('../middleware/auth')
+
+
+
+route.post('/register', authController.register)
+route.post('/login', authController.login)
+
+route.get('/logout', authController.logout)
+
+route.get('/refreshToken', authController.refereshToken)
+
+route.patch('/reset-password',auth, authController.resetPassword)
+
+module.exports = route
